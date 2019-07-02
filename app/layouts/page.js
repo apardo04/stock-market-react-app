210a8d8394257e80203c317e07bbd89c347806e4
apardo04 <apardo04@gmail.com>
2019-07-02 11:10:08 -0400

    adding google analytics code

--- app/layouts/page.js
+++ app/layouts/page.js
@@ -1,15 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { initGA, logPageView } from '../utils/analytics'
 import Head from '../components/Head'
 import Nav from '../components/Nav'
 
-const Page = ({ children }) => (
-    <Fragment>
-        <Head title="Stockify App"/>
-        <Nav />
-        <main>
-            {children}
-        </main>
-    </Fragment>
-);
+const Page = ({ children }) => {
+    useEffect(() => {
+        if (!window.GA_INITIALIZED && process.env.NODE_ENV == 'production') {
+            initGA()
+            window.GA_INITIALIZED = true
+        }
+        logPageView()
+      });
+    return (
+        <Fragment>
+            <Head title="Stockify App"/>
+            <Nav />
+            <main>
+                {children}
+            </main>
+        </Fragment>
+    );
+}
 
 export default Page;
