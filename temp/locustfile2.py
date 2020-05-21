5101484d4b6822070b9934e5eb68595e957bb638
apardo04 <apardo04@gmail.com>
2020-05-21 16:11:07 -0400

    testing different site

--- temp/locustfile2.py
+++ temp/locustfile2.py
@@ -3,7 +3,7 @@ from locust import HttpUser, TaskSet, task, between
 class UserTasks(TaskSet):
 	@task(1)
 	def index(self):
-		self.client.get("/r/investing")
+		self.client.get("/games/the-last-of-us-2")
 
 class WebsiteUser(HttpUser):
 	tasks = [UserTasks]
